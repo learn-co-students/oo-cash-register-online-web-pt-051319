2a4c7cf5f7119c2b1898391f559db0335f63d5fa
Mark May <mamark90@gmail.com>
2019-06-11 23:18:31 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :cash_register, :cash_register_with_discount
+
+  def initialize(total =0 )
+    @total = total
+    self.cash_register_with_discount(int)
+    binding.pry
+  end
+
+end
