92bb82743e01f80ed09085176bd873d62567ca4d
Annia Forney <anniaforney@gmail.com>
2019-06-19 19:31:48 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+require 'pry'
+class CashRegister
 
+  attr_accessor :total, :discount, :items
+
+  def initialize(total = 0)
+    @total = 0
+    @discount = 20
+  end
+
+  def add_item(title, price , total)
+    self.total += 1 
+  end
+end
