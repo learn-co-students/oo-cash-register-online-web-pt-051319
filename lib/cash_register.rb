cdd8ce988d2ece50e8fec7fc7c96f55e24f61b8a
Annia Forney <anniaforney@gmail.com>
2019-06-19 20:25:23 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class CashRegister
 
-  attr_accessor :total, :discount, :items
+  attr_accessor :total, :discount, :items, :apply_discount
 
   def initialize(total = 0)
     @total = 0
@@ -13,7 +13,7 @@ class CashRegister
   end
 
   def apply_discount
-
+     
   end
 
 end
